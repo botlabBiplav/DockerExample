a23552b1161d08dfa5260e947dc48bba4d27e05e
botlabBiplav <biplav.mazumdar@botlabdynamics.com>
2025-03-24 18:04:43 +0530

    cicd added

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ export default function Home() {
     <div className="flex justify-center items-center mt-20">
       <div className="">
       <h1>This is Docker Tutorial </h1>
+      <h2>There i have added the cicd</h2>
       </div>
       
     </div>
