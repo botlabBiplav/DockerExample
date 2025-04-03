ece9c783764753d9a0e5be461a7142e6247f0a60
AmmuRawat <amanrawat22081995@gmail.com>
2025-04-03 17:36:01 +0530

    Editing the heading

--- src/app/Hero.jsx
+++ src/app/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Hero = () => {
   return (
     <div>
-      <h1>This is Hero Card</h1>
+      <h1>This is Testing Hero Card By Tester</h1>
       <h3>This is a Test case we are going to check that test case is running or not </h3>
     </div>
   )
