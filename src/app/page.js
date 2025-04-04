d0fa6209df3e741e730b4b33f045106259830044
botlabBiplav <biplav.mazumdar@botlabdynamics.com>
2025-04-04 18:21:34 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -5,10 +5,8 @@ export default function Home() {
     <div className="flex justify-center items-center mt-20">
       <div className=" bg-green-300 p-5 text-center" id="document-content">
         <h4>Hello World</h4>
-      <h1>This is Docker Tutorial </h1>
-      <h2>There i have added the cicd</h2>
+        <h1>This is Docker Tutorial </h1>
       </div>
-      
     </div>
   );
 }
