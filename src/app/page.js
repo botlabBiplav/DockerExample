5abb7b5fb176b62a50de3d2e44d6d1dd9d127f41
botlabBiplav <biplav.mazumdar@botlabdynamics.com>
2025-04-04 18:16:48 +0530

    test ui added

--- src/app/page.js
+++ src/app/page.js
@@ -5,12 +5,8 @@ export default function Home() {
     <div className="flex justify-center items-center mt-20">
       <div className=" bg-green-300 p-5 text-center" id="document-content">
         <h4>Hello World</h4>
-      <h1>This is Docker Tutorial </h1>
-      <h2>There i have added the cicd</h2>
-      <h4>changes are there </h4>
-      <h5>This is a cicd changes </h5>
+        <h1>This is Docker Tutorial </h1>
       </div>
-      
     </div>
   );
 }
