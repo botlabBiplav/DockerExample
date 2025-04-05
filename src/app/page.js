f140268467b9f024d45537f5cf583a094155b6bd
botlabBiplav <biplav.mazumdar@botlabdynamics.com>
2025-04-05 12:03:17 +0530

    color added

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
       <div className=" bg-green-300 p-5 text-center" id="document-content">
         <h4>Hello World</h4>
         <h1>This is Docker Tutorial </h1>
-        <h3>We are checking the docker cicd </h3>
+        <h3 className="bg-red-500">We are checking the docker cicd </h3>
       </div>
     </div>
   );
