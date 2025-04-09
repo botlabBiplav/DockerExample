060a31097c76610219c7117d76f1d50199e74764
AmmuRawat <amanrawat22081995@gmail.com>
2025-04-09 17:46:58 +0530

    add heding2

--- src/app/Hero.jsx
+++ src/app/Hero.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Hero = () => {
   return (
     <div>
-      <h1>This is Testing Hero Card By Tester</h1>
+      <h1>This is Hero Card </h1>
+      <h2> This is Test2</h2>
       <h3>This is a Test case we are going to check that test case is running or not </h3>
     </div>
   )
